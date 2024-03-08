1e1035ef902483536a735336b38f1d0cec2efa19
DylanRigney <84356573+DylanRigney@users.noreply.github.com>
2024-03-08 00:01:58 -0500

    Setup Authentication w/ Clerk

---
+++ src/app/predictions/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sage - Predictions",
+};
+
+export default function Predictions() {
+  return <div>Predictions go here</div>;
+}
